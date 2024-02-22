73bf5c21aa0a7cd49a1a615ff056bc22a3703b5f
eva <evaorange73@gmail.com>
2024-02-22 05:22:09 +0300

    vue table

---
+++ src/main/resources/static/scripts/task-page.js
@@ -0,0 +1,11 @@
+
+
+
+var table = new Vue({
+    el: '#table',
+    data: {
+        points: [
+            {id: 0, x: 1, y: 1, r: 1, result: false, requestTime: 0, processTime: 0}
+        ]
+    }
+})
